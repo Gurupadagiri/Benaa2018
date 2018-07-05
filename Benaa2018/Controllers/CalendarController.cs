7965612b937ee0434d1f4c9fd296a4543289e87c
Gurupadagiri <gurugiri11@gmail.com>
2018-07-06 01:59:42 +0530

    calendar page
    calendar page

--- Benaa2018/Controllers/CalendarController.cs
+++ Benaa2018/Controllers/CalendarController.cs
@@ -7,20 +7,53 @@ using Benaa2018.Helper;
 
 namespace Benaa2018.Controllers
 {
-    public class CalendarController : Controller
+    public class CalendarController : BaseController
     {
         private readonly IMenuMasterHelper _menuMasterHelper;
+        private readonly IOwnerMasterHelper _ownerMasterHelper;
+        private readonly IProjectColorHelper _projectColorHelper;
+        private readonly IProjectGroupHelper _projectGroupHelper;
         private readonly IProjectMasterHelper _projectMasterHelper;
-        public CalendarController(IMenuMasterHelper menuMasterHelper, IProjectMasterHelper projectMasterHelper)
+        private readonly IProjectScheduleMasterHelper _projectScheduleMasterHelper;
+        private readonly IProjectStatusMasterHelper _projectStatusMasterHelper;
+        private readonly ISubContractorHelper _subContractorHelper;
+        private readonly IUserMasterHelper _userMasterHelper;
+        private readonly ICompanyMasterHelper _companyMasterHelper;
+        public CalendarController(IMenuMasterHelper menuMasterHelper,
+            IOwnerMasterHelper ownerMasterHelper,
+            IProjectColorHelper projectColorHelper,
+            IProjectGroupHelper projectGroupHelper,
+            IProjectMasterHelper projectMasterHelper,
+            IProjectScheduleMasterHelper projectScheduleMasterHelper,
+            IProjectStatusMasterHelper projectStatusMasterHelper,
+            ISubContractorHelper subContractorHelper,
+            IToDoMasterHelper toDoMasterHelper,
+            IUserMasterHelper userMasterHelper,
+            IWarrentyAlertHelper warrentyAlertHelper,
+            ICompanyMasterHelper companyMasterHelper) : base(menuMasterHelper,
+            ownerMasterHelper,
+            projectColorHelper,
+            projectGroupHelper,
+            projectMasterHelper,
+            projectScheduleMasterHelper,
+            projectStatusMasterHelper,
+            subContractorHelper,
+            userMasterHelper,
+            companyMasterHelper)
         {
             _menuMasterHelper = menuMasterHelper;
+            _ownerMasterHelper = ownerMasterHelper;
+            _projectColorHelper = projectColorHelper;
+            _projectGroupHelper = projectGroupHelper;
             _projectMasterHelper = projectMasterHelper;
+            _projectScheduleMasterHelper = projectScheduleMasterHelper;
+            _projectStatusMasterHelper = projectStatusMasterHelper;
+            _subContractorHelper = subContractorHelper;
+            _userMasterHelper = userMasterHelper;
+            _companyMasterHelper = companyMasterHelper;
         }
         public async Task<IActionResult> Index()
         {
-            ViewBag.MenuDetails = await _menuMasterHelper.GetMenuItems();
-            ViewBag.ProjectMasterModels = await _projectMasterHelper.GetAllProjectByUserId(1);
-            ViewBag.ProjectManagerMasterModels = await _projectMasterHelper.GetAllManagers();
             return View();
         }
     }
