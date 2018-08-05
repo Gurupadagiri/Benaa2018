90476e0f22665b45120ed54fe2b6e99676b9cb8c
Gurupadagiri <gurugiri11@gmail.com>
2018-08-05 22:12:52 +0530

    calendar

---
+++ Benaa2018.Data.Helpers/Model/CalendarGanttChart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benaa2018.Helper.Model
+{
+    public class CalendarGanttChart
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Duration { get; set; }
+        public decimal Progress { get; set; }
+        public int? Predicate { get; set; }
+        public string AssignedTo { get; set; }
+        public bool Status { get; set; }
+    }
+}
