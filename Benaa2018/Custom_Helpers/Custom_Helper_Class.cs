3427e3540dff321094bbb172c7e6549e27166772
Sumit Ganguly <sumitganguly1989@gmail.com>
2018-09-16 01:24:24 +0530

    todo insert

---
+++ Benaa2018/Custom_Helpers/Custom_Helper_Class.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace Benaa2018.Custom_Helpers
+{
+    public static class Custom_Helper_Class
+    {
+        public static HtmlString Create_Lable(string Content)
+            {
+                string LableStr = "<label class=\"lblStatus\" style=\"background-color:gray;color:yellow;font-size:24px\">{Content}</label>";
+                return new HtmlString(LableStr);
+            }
+
+        //public static HtmlString Create_DropDown(string Content)
+        //{
+        //    //string LableStr = "<label class=\"lblStatus\" style=\"background-color:gray;color:yellow;font-size:24px\">{Content}</label>";
+        //   // string ddlUser= "<select asp-for="lstCheckListDetail[Model.lstCheckListDetail.IndexOf(item)].ToDoCheckListUserId" class="ddldropdown">"
+        //    return new HtmlString(LableStr);
+        //}
+
+    }
+}
