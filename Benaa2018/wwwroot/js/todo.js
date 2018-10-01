1a604d0fe7ce76d8ddad539355a1487093707613
Gurupadagiri <gurugiri11@gmail.com>
2018-10-01 11:02:46 +0530

    todo
    todo

--- Benaa2018/wwwroot/js/todo.js
+++ Benaa2018/wwwroot/js/todo.js
@@ -26,13 +26,6 @@ $(document).ready(function () {
             $('#ddlTag').multiselect();
         });
     });
-    //$(document).on("click", ".typeTitle", function () {
-    //    var ids = $(this).attr('data-ids');
-    //    var postData = { "ToDoDetailsId": parseInt(ids) };
-    //    $("#todoinfoModal").load('ToDo/PopulateTodoInfo', postData, function (result) {
-    //        $('#todoinfoModal').modal('show');
-    //    });
-    //});
     $(document).on('click', '#btnAddExtraUpdate', function (e) {
         for (var j = 3; j < 6; j++) {
             var index = 'cloneingboxholder' + j;
