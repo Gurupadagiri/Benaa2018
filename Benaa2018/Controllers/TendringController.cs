609f3376b22ec0584648b653b79009e488eaba13
Sumit Ganguly <sumitganguly1989@gmail.com>
2018-10-27 22:37:27 +0530

    tendering page
    tendering page

---
+++ Benaa2018/Controllers/TendringController.cs
@@ -0,0 +1,88 @@
+﻿using Benaa2018.Helper;
+using Benaa2018.Helper.Interface;
+using Benaa2018.Helper.Model;
+using Benaa2018.Helper.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benaa2018.Controllers
+{
+    public class TendringController : BaseController
+    {
+        private readonly IMenuMasterHelper _menuMasterHelper;
+        private readonly IOwnerMasterHelper _ownerMasterHelper;
+        private readonly IProjectColorHelper _projectColorHelper;
+        private readonly IProjectGroupHelper _projectGroupHelper;
+        private readonly IProjectMasterHelper _projectMasterHelper;
+        private readonly IProjectScheduleMasterHelper _projectScheduleMasterHelper;
+        private readonly IProjectStatusMasterHelper _projectStatusMasterHelper;
+        private readonly ISubContractorHelper _subContractorHelper;
+        private readonly IUserMasterHelper _userMasterHelper;
+        private readonly ICompanyMasterHelper _companyMasterHelper;
+        private readonly IToDoMasterDetailsHelper _todoMasterDetailsHelper;
+        private readonly ITagMasterHelper _tagMasterHelper;
+        private readonly IToDoAssignHelper _toDoAssignHelper;
+        private readonly IToDoTagHelper _tagToDoHelper;
+        private readonly IToDoCheckListHelper _toDoCheckListHelper;
+        private readonly IToDoCheckListDetailsHelper _toDoCheckListDetailsHelper;
+        private readonly IToDoMessageHelper _toDoMessageHelper;
+        private readonly ICalendarScheduleHelper _calendarScheduleHelper;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public TendringController(IMenuMasterHelper menuMasterHelper,
+            IOwnerMasterHelper ownerMasterHelper,
+            IProjectColorHelper projectColorHelper,
+            IProjectGroupHelper projectGroupHelper,
+            IProjectMasterHelper projectMasterHelper,
+            IProjectScheduleMasterHelper projectScheduleMasterHelper,
+            IProjectStatusMasterHelper projectStatusMasterHelper,
+            ISubContractorHelper subContractorHelper,
+            IToDoMasterHelper toDoMasterHelper,
+            IUserMasterHelper userMasterHelper,
+            IWarrentyAlertHelper warrentyAlertHelper,
+            IToDoMasterDetailsHelper tomasterDetails,
+            ITagMasterHelper tagMasterHelper,
+            IToDoTagHelper toDoTagHelper,
+            IToDoAssignHelper toDoAssignHelper,
+            IToDoCheckListHelper toDoCheckListHelper,
+            IToDoCheckListDetailsHelper toDoCheckListDetailsHelper,
+            IToDoMessageHelper toDoMessageHelper,
+            ICompanyMasterHelper companyMasterHelper, ICalendarScheduleHelper calendarScheduleHelper, IHostingEnvironment hostingEnvironment) : base(menuMasterHelper,
+            ownerMasterHelper, projectColorHelper, projectGroupHelper, projectMasterHelper, projectScheduleMasterHelper, projectStatusMasterHelper,
+            subContractorHelper,
+            userMasterHelper,
+            companyMasterHelper)
+        {
+            _menuMasterHelper = menuMasterHelper;
+            _ownerMasterHelper = ownerMasterHelper;
+            _projectColorHelper = projectColorHelper;
+            _projectGroupHelper = projectGroupHelper;
+            _projectMasterHelper = projectMasterHelper;
+            _projectScheduleMasterHelper = projectScheduleMasterHelper;
+            _projectStatusMasterHelper = projectStatusMasterHelper;
+            _subContractorHelper = subContractorHelper;
+            _userMasterHelper = userMasterHelper;
+            _companyMasterHelper = companyMasterHelper;
+            _todoMasterDetailsHelper = tomasterDetails;
+            _tagMasterHelper = tagMasterHelper;
+            _toDoAssignHelper = toDoAssignHelper;
+            _tagToDoHelper = toDoTagHelper;
+            _toDoCheckListHelper = toDoCheckListHelper;
+            _toDoCheckListDetailsHelper = toDoCheckListDetailsHelper;
+            _toDoMessageHelper = toDoMessageHelper;
+            _calendarScheduleHelper = calendarScheduleHelper;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var tagsList = await GetAllTags();
+            return View();
+        }
+    }
+}
